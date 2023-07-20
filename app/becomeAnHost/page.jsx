f3ee5a8f64b0473f36543c4aae8068633d416e10
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2023-07-20 21:30:12 +0530

    Update page.jsx

--- app/becomeAnHost/page.jsx
+++ app/becomeAnHost/page.jsx
@@ -1,10 +1,7 @@
 "use client";
 import React, { useState } from "react";
 
-export const metadata = {
-  title: "Become an Host",
-  description: "Meet and Get Guided",
-};
+
 
 export default function Example() {
   const [name, setName] = useState("");
